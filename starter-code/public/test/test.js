3b5b1eb4c02f6d584a9f3406675aa2f9aa3a8ee8
nicky evers <livelyevers@gmail.com>
2017-03-17 14:54:04 -0700

    first 4 todo's done

--- starter-code/public/test/test.js
+++ starter-code/public/test/test.js
@@ -1,21 +1,46 @@
 QUnit.module('Article Constructor', function() {
-  QUnit.test('Article should create a new Arcticle', function(assert) {
-    //TODO: write a test for the Article constructor
+  QUnit.test('Article should create a new Article', function(assert) {
+    //DONE TODO: write a test for the Article constructor
+    
+    let testArticle = {
+      title: "Transmitting Open-source Arrays",
+      category: "firewall",
+      author: "Dr. Tressie Kuphal",
+      authorUrl: "http://http://corrine.net",
+      publishedOn: "2014-01-22",
+      body: "## The RSS sensor is down, synthesize the open-source sensor so we can synthesize the RSS sensor!\n\ntransmitting the driver won't do anything."
+    }
+    let newArticle = new Article(testArticle)
+
+    assert.equal(newArticle.publishedOn, "2014-01-22", 'made an article')
   });
 
   QUnit.test('Article instance should have a method toHtml', function(assert) {
-    //TODO: verify that an instance of article has a toHtml method
+    let testArticle = {}
+    let newArticle = new Article(testArticle)
+
+    assert.ok(newArticle.toHtml);
+    //DONE TODO: verify that an instance of article has a toHtml method
   });
 });
 
 QUnit.module('Article Class', function() {
   QUnit.test('Article.allArticles should be an array', function(assert) {
-    //TODO: write a test for the all articles property
+    //DONE TODO: write a test for the all articles property
+    assert.equal(Array.isArray(Article.all), true, 'is array')
   });
   QUnit.test('Article.loadAll should set an array of Article instances', function(assert) {
-    //TODO: write a test for article loadAll
+    //DONE TODO: write a test for article loadAll
     //remember to reset anything that changed about your global
     //environment at the end of the test!
+
+    // let testArticle = {}
+    // let newArticle = new Article(testArticle)
+
+    let testArticle = [{},{},{}];
+    Article.loadAll(testArticle);
+    assert.equal(Article.all.length > 0, true)
+    assert.equal(Article.all[0] instanceof Article, true)
   });
   QUnit.test('Article.allAuthors should get unique author names', function(assert) {
     //TODO: write a test for Article.allAuthors
@@ -23,3 +48,4 @@ QUnit.module('Article Class', function() {
   //STRETCH: write a test for Article.numWordsAll
   //STRETCH: write a test for Article.fetchAll
 })
+
